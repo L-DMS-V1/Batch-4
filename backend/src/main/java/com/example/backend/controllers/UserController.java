0641efb254dea7433a9aa2564dbd98226e170edc
mohitRauniyar <learning.mohitrauniyar.01@gmail.com>
2024-10-24 23:44:54 +0530

    add login api with jwt-token response

--- backend/src/main/java/com/example/backend/controllers/UserController.java
+++ backend/src/main/java/com/example/backend/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.example.backend.controllers;
 
+import com.example.backend.DTOs.JwtResponse;
+import com.example.backend.DTOs.UserLoginDto;
 import com.example.backend.DTOs.UserRegistrationDTO;
 import com.example.backend.models.User;
 import com.example.backend.services.UserService;
+import com.example.backend.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +23,10 @@ import static com.example.backend.utils.ValidationUtils.isEmptyField;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtUtil jwtUtil;
     @PostMapping("/register")
     public ResponseEntity<?> registerNewUser(@RequestBody UserRegistrationDTO userRegistrationDto, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
@@ -36,5 +44,15 @@ public class UserController {
         }catch(Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error during registration: " + e.getMessage());
         }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody UserLoginDto loginDto){
+        User user = userService.findByUsername(loginDto.getUsername());
+        if(user == null || !passwordEncoder.matches(loginDto.getPassword(), user.getPassword())){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid Credentials");
+        }
+        String token = jwtUtil.generateToken(user);
+        return ResponseEntity.status(HttpStatus.OK).body(new JwtResponse(token));
     }
 }
--- backend/src/main/java/com/example/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/example/backend/repositories/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByUsername(String username);
+
+    User findByUsername(String username);
 }
