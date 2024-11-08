ab34cbc239a2e500064c212d6a045402ab478420
mohitRauniyar <learning.mohitrauniyar.01@gmail.com>
2024-11-08 08:33:29 +0530

    add api integration for module1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './components/Home'
+import Signup from './components/Signup'
+import Login from './components/Login'
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </BrowserRouter>
   )
